a79ac312b3944130c01076dd0546ec4d307bdcf8
AkshadaPasalkar <akshadad71@gmail.com>
2021-10-02 01:45:26 -0700

    first commit

--- src/book/hotel/PasswordDetails.java
+++ src/book/hotel/PasswordDetails.java
@@ -13,10 +13,10 @@ public class PasswordDetails {
 		driver.get("http://adactinhotelapp.com/HotelAppBuild2/index.php");
 		
 		WebElement uname=driver.findElement(By.xpath("//input[@type='text']"));
-		uname.sendKeys("tquser123");
+		uname.sendKeys("akshadadeshmukh1");
 		
 		WebElement pass=driver.findElement(By.xpath("//input[@type='password']"));
-		pass.sendKeys("tquser1234");
+		pass.sendKeys("pppqqq");
 		String password=pass.getAttribute("value");
 		System.out.print("Your Password in Index Page is :"+password);
 		
@@ -27,10 +27,10 @@ public class PasswordDetails {
 		change.click();
 		
 		WebElement currentPass=driver.findElement(By.xpath("//input[@name='current_pass']"));
-		currentPass.sendKeys("tquser1234");
+		currentPass.sendKeys("pppqqq");
 		
 		String currentPassword=currentPass.getAttribute("value");
-		System.out.print("\nCurrent Password is: "+ currentPassword);
+		
 		
 		String newPassword;
 		String confirmPassword;
@@ -38,6 +38,7 @@ public class PasswordDetails {
 		//To verify user is changing the password using its current password
 		if(currentPassword.equals(password))
 		{
+			System.out.print("\nCurrent Password is: "+ currentPassword);
 		WebElement newPass=driver.findElement(By.xpath("//input[@name='new_password']"));
 		newPass.sendKeys("pqr1234");
 		newPassword=newPass.getAttribute("value");
@@ -46,21 +47,56 @@ public class PasswordDetails {
 		confirmPass.sendKeys("pqr1234");
 		confirmPassword=confirmPass.getAttribute("value");
 	
-		//WebElement btn1=driver.findElement(By.xpath("//input[@type='submit']"));
-		//btn1.click();
+		boolean a=newPassword.isEmpty();
+		boolean b=confirmPassword.isEmpty();
 		
 		System.out.print("\nPassword in index page is same as current password in Change Password page\n");
 		
-			if(newPassword.equals(confirmPassword))
+			if(newPassword.equals(confirmPassword) & a!=true & b!=true)
 			{
 				WebElement btn1=driver.findElement(By.xpath("//input[@type='submit']"));
 				btn1.click();
 				System.out.print("Your Password is Successfully updated!!!");
 			}
+			else if (newPassword.isEmpty() & confirmPassword.isEmpty()) 
+			{
+				System.out.println("Please Enter New Password and Confirm Password\n");
+				WebElement btn1=driver.findElement(By.xpath("//input[@type='submit']"));
+				btn1.click();
+			}
+			else if(newPassword.isEmpty()) 
+			{
+				System.out.print("Please Enter New Password");
+				WebElement btn1=driver.findElement(By.xpath("//input[@type='submit']"));
+				btn1.click();
+			}
+			else if (confirmPassword.isEmpty()) 
+			{
+				System.out.print("Please Enter Confirm Password");
+				WebElement btn1=driver.findElement(By.xpath("//input[@type='submit']"));
+				btn1.click();
+			}
+			
 			else 
 			{
 				System.out.print("\nYour New Password and Confirmation Password do not match");
+				WebElement btn1=driver.findElement(By.xpath("//input[@type='submit']"));
+				btn1.click();
 			}
+		}
+		else if (currentPassword.isEmpty())
+		{
+			WebElement newPass=driver.findElement(By.xpath("//input[@name='new_password']"));
+			newPass.sendKeys("pqr1234");
+			newPassword=newPass.getAttribute("value");
+			
+			WebElement confirmPass=driver.findElement(By.xpath("//input[@name='re_password']"));
+			confirmPass.sendKeys("pqr1234");
+			confirmPassword=confirmPass.getAttribute("value");
+			
+			WebElement btn1=driver.findElement(By.xpath("//input[@type='submit']"));
+			btn1.click();
+			System.out.println("\nPlease Enter Correct Current Password");
 		}
 		else
 		{
